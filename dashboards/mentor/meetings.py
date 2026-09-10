b7ea4016335dad3aaa0ead02e7074869acad07eb
ayeshamehreentech <ayeshamehreen.tech@gmail.com>
2026-09-10 15:31:36 +0530

    Add mentor meeting delete button

--- dashboards/mentor/meetings.py
+++ dashboards/mentor/meetings.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import streamlit.components.v1 as components
 from ai.config import get_setting
-from database.meetings import list_meetings, save_meeting, save_recurring_meetings, update_meeting
+from database.meetings import delete_meeting, list_meetings, save_meeting, save_recurring_meetings, update_meeting
 from database.users import list_users
 
 
@@ -15,9 +15,12 @@ def render_mentor_meetings():
         title = st.text_input("Meeting title", key="meeting_title")
         meeting_date = st.date_input("First meeting date", key="meeting_date")
         hour, minute, meridiem = st.columns(3)
-        with hour: value_hour = st.selectbox("Hour", list(range(1,13)), index=8, key="meeting_hour")
-        with minute: value_minute = st.selectbox("Minute", [0,15,30,45], key="meeting_minute")
-        with meridiem: value_meridiem = st.selectbox("AM / PM", ["AM","PM"], index=1, key="meeting_meridiem")
+        with hour:
+            value_hour = st.selectbox("Hour", list(range(1, 13)), index=8, key="meeting_hour")
+        with minute:
+            value_minute = st.selectbox("Minute", [0, 15, 30, 45], key="meeting_minute")
+        with meridiem:
+            value_meridiem = st.selectbox("AM / PM", ["AM", "PM"], index=1, key="meeting_meridiem")
         time = "{:02d}:{:02d} {}".format(value_hour, value_minute, value_meridiem)
         link = st.text_input("Meeting link", value=get_setting("TEAMS_MEETING_LINK"), key="meeting_link")
         repeat = st.checkbox("Repeat every Tuesday and Thursday (next 12 meetings)", value=True, key="meeting_repeat_tue_thu")
@@ -26,26 +29,37 @@ def render_mentor_meetings():
                 st.error("Select a student and enter a title and meeting link.")
             elif repeat:
                 save_recurring_meetings(student["id"], st.session_state.get("user_id"), title, meeting_date, time, link)
-                st.success("12 Tuesday/Thursday meetings scheduled."); st.rerun()
+                st.success("12 Tuesday/Thursday meetings scheduled.")
+                st.rerun()
             else:
                 save_meeting(student["id"], st.session_state.get("user_id"), title, meeting_date, time, link)
-                st.success("Meeting scheduled."); st.rerun()
+                st.success("Meeting scheduled.")
+                st.rerun()
     meetings = list_meetings(st.session_state.get("user_id"), role="mentor")
-    if not meetings: st.info("No meetings scheduled.")
+    if not meetings:
+        st.info("No meetings scheduled.")
     for meeting in meetings:
         with st.container(border=True):
             st.markdown("### " + meeting.get("title", "Meeting"))
             st.write("Date: {} · Time: {}".format(meeting.get("meeting_date", "N/A"), meeting.get("meeting_time", "N/A")))
             link = meeting.get("meeting_link", "")
-            if link:
-                if st.button("🖥️ Show meeting here", key="mentor_show_{}".format(meeting["id"])):
-                    st.session_state["active_mentor_meeting_link"] = link; st.rerun()
+            open_col, delete_col = st.columns(2)
+            with open_col:
+                if link and st.button("🖥️ Show meeting here", key="mentor_show_{}".format(meeting["id"])):
+                    st.session_state["active_mentor_meeting_link"] = link
+                    st.rerun()
+            with delete_col:
+                if st.button("🗑️ Delete meeting", key="delete_meeting_{}".format(meeting["id"]), type="secondary"):
+                    delete_meeting(meeting["id"])
+                    st.success("Meeting deleted.")
+                    st.rerun()
             with st.expander("Edit meeting"):
                 changed_title = st.text_input("Title", meeting.get("title", ""), key="edit_title_{}".format(meeting["id"]))
                 changed_date = st.text_input("Date", meeting.get("meeting_date", ""), key="edit_date_{}".format(meeting["id"]))
                 changed_time = st.text_input("Time", meeting.get("meeting_time", ""), key="edit_time_{}".format(meeting["id"]))
                 changed_link = st.text_input("Meeting link", link, key="edit_link_{}".format(meeting["id"]))
                 if st.button("Save meeting changes", key="save_meeting_{}".format(meeting["id"])):
-                    update_meeting(meeting["id"], changed_title, changed_date, changed_time, changed_link); st.rerun()
+                    update_meeting(meeting["id"], changed_title, changed_date, changed_time, changed_link)
+                    st.rerun()
     if st.session_state.get("active_mentor_meeting_link"):
         components.iframe(st.session_state["active_mentor_meeting_link"], height=520, scrolling=True)
